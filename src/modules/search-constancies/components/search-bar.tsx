d70c8800626a1be16fe59dc5efee1c79d4ddf1e7
Lisandro <salvareschilisandro0@gmail.com>
2024-03-03 12:57:22 -0300

    style search bar

--- src/modules/search-constancies/components/search-bar.tsx
+++ src/modules/search-constancies/components/search-bar.tsx
@@ -88,7 +88,7 @@ export default function SearchBarComponent({ setResults, setLoading }: { setResu
                         <input
                             type="search"
                             id="search-dropdown"
-                            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 border-gray-50 border-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 outline-none"
+                            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 border-gray-50 border-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 outline-none rounded-none"
                             placeholder="Ingresé su CURP o número de folio"
                             required
                             onChange={(e: any) => setString(e.target.value)}
